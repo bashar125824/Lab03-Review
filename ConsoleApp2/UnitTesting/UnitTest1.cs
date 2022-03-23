73e2777bf378884b5c2aa85192f977e23a153f44
bashar125824 <bashar.alrefae.7@gmail.com>
2022-03-24 01:18:18 +0300

    Edit Challenge 2 and challenge 8

---
+++ ConsoleApp2/UnitTesting/UnitTest1.cs
@@ -0,0 +1,19 @@
+using System;
+using Xunit;
+using ConsoleApp2;
+
+namespace ConsoleApp2
+
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+            // Enter 1 2 and 3 as inputs , the product should be 6 
+           
+
+        }
+    }
+}
